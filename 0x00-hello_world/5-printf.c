212d33d34240fab44f771a82dc93d0fe35244142
Byiringiro Seraphin <seraphinbyiringiro112@gmail.com>
2023-06-16 12:13:59 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * mainn -  program that print a line using printf function
  * Return: 0 (Success)
- */
+ *i/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
