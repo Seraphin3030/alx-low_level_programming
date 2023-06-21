94454ec2120eb0ad073f09ec26787124138c303e
Byiringiro Seraphin <seraphinbyiringiro112@gmail.com>
2023-06-21 14:33:51 -0700

    a

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _is slower - function to check if character is lowercase
+ *
+ * 0c: check input function
+ *
+ * Return: return 1 if 'c' is lowercase
+ *         otherwise 0 is (Success)
+ */
+int _islower(int C)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 1 int _putchar(char c);
 2 void print_alphabet(void);
 3 void print_alphabet_x10(void);
+4 int _islower(int c); 
